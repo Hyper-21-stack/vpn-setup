ca01d1327101576ec149e89f559ad6a736b19488
Hyper-21-stack <saunderstobin25@gmail.com>
2024-12-30 02:49:14 -0800

    Update Install-vpn.sh

--- Install-vpn.sh
+++ Install-vpn.sh
@@ -142,7 +142,7 @@ if [[ ! -e /etc/wireguard/wg0.conf ]]; then
         fi
         clear
         figlet -kE *MTN* | lolcat
-        echo -e "\033[1;33mResleeved Net Wireguard\033[0m"
+        echo -e "\033[1;33mHyper WireGuard\033[0m"
         # If system has a single IPv4, it is selected automatically. Else, ask the user
         if [[ $(ip -4 addr | grep inet | grep -vEc '127(\.[0-9]{1,3}){3}') -eq 1 ]]; then
                 ip=$(ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}' | cut -d '/' -f 1 | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}')
